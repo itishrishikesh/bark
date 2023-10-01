0854e3291bc3d9a6cadcbf98ae003e4329eae954
Hrishikesh Patil <hrishikesh.patil@yandex.com>
2023-10-01 22:45:42 +0530

    tests

--- tests/network_test.go
+++ tests/network_test.go
@@ -2,7 +2,6 @@ package tests
 
 import (
 	"github.com/techrail/bark/client"
-	"github.com/techrail/bark/models"
 	"testing"
 )
 
@@ -10,22 +9,22 @@ func Test_requester(t *testing.T) {
 	logClient := client.NewClient("http://localhost:8080/", "INFO", "ServicName", "localRun")
 
 	// Print with formatter
-	logClient.Error("L#1L3WBF - Anime: Naruto")
-	logClient.Info("L#1L3WBF Anime: One Piece")
-	logClient.Debug(" -- Anime: Bleach")
-	logClient.Warn("-- Anime: AOT")
+	logClient.Error("E#1L3WBF - Anime: Naruto")
+	logClient.Info("I#1L3WBF - Anime: One Piece")
+	logClient.Debug("D#1L3WBF - Anime: Bleach")
+	logClient.Warn("W#1L3WBF - Anime: AOT")
 
 	// Print without formatter
-	logClient.Errorf("L#1L3WBF - Anime: %s", "Full Metal Alchemist")
-	logClient.Infof("L#1L3WBF - Anime: %s", "Tokyo Ghoul")
-	logClient.Warnf("L#1L3WBF - Anime: %s", "")
-	logClient.Debugf("L#1L3WBF - I want to print something! %s", "weirdString")
+	logClient.Errorf("E#1L3WBF - Anime: %s", "Full Metal Alchemist")
+	logClient.Infof("I#1L3WBF - Anime: %s", "Tokyo Ghoul")
+	logClient.Warnf("W#1L3WBF - Anime: %s", "Haikyuu")
+	logClient.Debugf("D#1L3WBF - Anime: %s", "Demon Slayer")
 
 	// Multiple Logs
-	var logs []models.BarkLog
-	logs = make([]models.BarkLog, 3)
-	logs[0] = models.BarkLog{Message: "someMessage"}
-	logs[1] = models.BarkLog{Message: "someMessage"}
-	logs[2] = models.BarkLog{Message: "someMessage"}
-	_, _ = client.PostLogArray("http://localhost:8080/insertMultiple", logs)
+	//var logs []models.BarkLog
+	//logs = make([]models.BarkLog, 3)
+	//logs[0] = models.BarkLog{Message: "someMessage"}
+	//logs[1] = models.BarkLog{Message: "someMessage"}
+	//logs[2] = models.BarkLog{Message: "someMessage"}
+	//_, _ = client.PostLogArray("http://localhost:8080/insertMultiple", logs)
 }
